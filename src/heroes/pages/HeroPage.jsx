59877a349e0fdf1388ccb472eda633192815d7ca
Andres Felipe Villarraga Arango <avillarraga@opc.com.co>
2023-07-17 15:33:39 -0500

    Se avanza con la logica de busqueda

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,5 +1,6 @@
 import { Navigate, useNavigate, useParams } from "react-router-dom"
 import { getHeroById } from "../helpers"
+import { useMemo } from "react"
 
 
 
@@ -9,7 +10,7 @@ export const HeroPage = () =>{
 
     const { id } = useParams()
 
-    const hero = getHeroById( id )
+    const hero = useMemo(() => getHeroById( id ), [ id ])
 
     const { 
             superhero,
@@ -28,14 +29,14 @@ export const HeroPage = () =>{
                     <img 
                         src={`/src/assets/heroes/${ id }.jpg`}
                         alt={ superhero }
-                        className="img-thumbnail" />
+                        className="img-thumbnail animate__animated animate__fadeInLeft" />
                 </div>
                 <div className="col-8">
                     <h3>{ superhero }</h3>
                     <ul className="list-group list-group-flush">
-                        <li className="list-group-item"><b>Alter ego:</b>{alter_ego}</li>
-                        <li className="list-group-item"><b>Publisher</b>{publisher}</li>
-                        <li className="list-group-item"><b>First appearance</b>{first_appearance}</li>
+                        <li className="list-group-item"><b>Alter ego: </b>{alter_ego}</li>
+                        <li className="list-group-item"><b>Publisher: </b>{publisher}</li>
+                        <li className="list-group-item"><b>First appearance: </b>{first_appearance}</li>
                     </ul>
 
                     <h5 className="mt-4">Characters</h5>
--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,5 +1,73 @@
-
-
+import { useLocation, useNavigate } from 'react-router-dom'
+import queryString from 'query-string'
+import { useForm } from '../../hooks/useForm'
+import { HeroCard } from '../components'
+import { getHeroByName } from '../helpers'
 export const SearchPage = () =>{
-    return (<h1>SearchPage</h1>)
+    const navigate = useNavigate()
+
+    const location = useLocation()
+    
+    const { q = '' } = queryString.parse(location.search)
+
+    const { searchText, onInputChange } = useForm({
+        searchText:''
+    })
+
+    const onSearchSummit = (event) =>{
+        event.preventDefault()
+        navigate(`?q=${ searchText }`)
+    }
+
+    const heroes = getHeroByName(q)
+
+    const showSearch = q.length === 0
+    const showError = q.length > 0 && heroes.length === 0
+
+    return (
+        <>
+            <h1>SearchPage</h1>
+            <hr />
+
+            <div className="row">
+                <div className="col-5">
+                    <h4>Searching</h4>
+                    <hr />
+                    <form onSubmit={ onSearchSummit }>
+                        <input 
+                            type="text"
+                            name="searchText"
+                            placeholder="Search a hero"
+                            className="form-control"
+                            autoComplete="off"
+                            value={ searchText }
+                            onChange={ onInputChange }
+                        />
+                        <button className="btn btn-outline-primary mt-1">
+                            Search
+                        </button>
+                    </form>
+                </div>
+                <div className="col-7">
+                    <div 
+                        className="alert alert-primary animate__animated animate__fadeIn"
+                        style={{display: showSearch ? '' : 'none'}}
+                        >
+                        Search a hero 
+                    </div>
+
+                    <div 
+                        className="alert alert-danger animate__animated animate__fadeIn"
+                        style={{display: showError ? '':'none'}}>
+                        No hero with <b>{q}</b>
+                    </div>
+
+                    {
+                        heroes.map(h => <HeroCard key={h.id} { ...h }/> )
+                    }
+                </div>
+            </div>
+
+        </>
+    )
 }
